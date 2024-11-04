792203010740a7e09532082cf44f5e85bace87ca
csmith120 <taskmaster3654@gmail.com>
2024-11-04 13:16:37 -0600

    4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { TasksComponent } from './tasks/tasks.component';
+
+export const routes: Routes = [{
+    path: 'tasks',
+    component: TasksComponent,
+},];
